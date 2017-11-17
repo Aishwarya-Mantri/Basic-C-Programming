c26e02d8a3202dfd774ee6614e976bb4475dd722
Aish Mantri <aishwaryamantri04@gmail.com>
2017-11-17 11:55:53 +0530

    Add files via upload
    Matrix Multiplication

--- main.c
+++ main.c
@@ -1,37 +1,60 @@
-//C Program To SORT String in Alphabetical Order
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 3 // Size of the matrix
 int main()
 {
-   int i,j;
-   char str[5][50],temp[50];
-   printf("\n enter 5 string \n");
-   for(i=0;i<5;i++)
-   {
-       gets(str[i]);
-   }
-  printf("\nData:\n");
-   for(i=0;i<5;i++)
-   {
-       printf("%s \t",str[i]);
-   }
-  printf("\n\nAlphabetical Order:\n");
-   for(i=0;i<5;i++)
-   {
-       for(j=i+1;j<5;j++)
-       {
-           if(strcmp(str[i], str[j])>0)
-            {
-                strcpy(temp, str[i]);
-                strcpy(str[i], str[j]);
-                strcpy(str[j], temp);
-            }
-       }
-   }
-  for(i=0; i<5; i++)
+    printf("Matrix Multiplication!\n");
+    int A[SIZE][SIZE]; // Matrix 1
+    int B[SIZE][SIZE]; // Matrix 2
+    int C[SIZE][SIZE]; // Resultant matrix
+    int row, col, k, sum;
+/* Input elements in first matrix from user */
+
+printf("Enter elements in matrix A of size %dx%d: \n", SIZE, SIZE);
+    for(row=0; row<SIZE; row++)
     {
-        puts(str[i]);
+        for(col=0; col<SIZE; col++)
+        {
+            scanf("%d", &A[row][col]);
+        }
     }
-printf("\n\n");
-    return 0;
+
+    /* Input elements in second matrix from user */
+    printf("\nEnter elements in matrix B of size %dx%d: \n", SIZE, SIZE);
+    for(row=0; row<SIZE; row++)
+    {
+        for(col=0; col<SIZE; col++)
+        {
+            scanf("%d", &B[row][col]);
+        }
+    }
+  for(row=0; row<SIZE; row++)   //Multiply both matrices A*B
+    {
+        for(col=0; col<SIZE; col++)
+        {
+            sum = 0;
+            /*
+             * Multiply row of first matrix to column of second matrix
+             * and store sum of product of elements in sum.
+             */
+            for(k=0; k<SIZE; k++)
+            {
+                sum =sum+ A[row][k] * B[k][col];
+            }
+
+            C[row][col] = sum;
+        }
+    }
+
+    /* Print product of the matrices */
+    printf("\nProduct of matrix A * B = \n");
+    for(row=0; row<SIZE; row++)
+    {
+        for(col=0; col<SIZE; col++)
+        {
+            printf("%d ", C[row][col]);
+        }
+        printf("\n");
+    }
+return 0;
 }
