e611bf747b653878ed37ce0eaee60cb9c4bd9e35
Aish Mantri <aishwaryamantri04@gmail.com>
2017-12-13 15:11:50 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,60 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 3 // Size of the matrix
+
 int main()
 {
-    printf("Matrix Multiplication!\n");
-    int A[SIZE][SIZE]; // Matrix 1
-    int B[SIZE][SIZE]; // Matrix 2
-    int C[SIZE][SIZE]; // Resultant matrix
-    int row, col, k, sum;
-/* Input elements in first matrix from user */
+   printf("\n    Program to print first nonrepetitive element of array \n ");
+   printf("__________________________________________________________________");
+   int arr[8]={2,4,7,9,4,6,2,4};
+   int i,j,count[8]={0};
+   printf("\n Array elements are:\n");
+   for(i=0;i<8;i++)
+   {
+       printf("%4d",arr[i]);
 
-printf("Enter elements in matrix A of size %dx%d: \n", SIZE, SIZE);
-    for(row=0; row<SIZE; row++)
-    {
-        for(col=0; col<SIZE; col++)
-        {
-            scanf("%d", &A[row][col]);
-        }
-    }
+   }
+   for(i=0;i<8;i++)
+   {
+       for(j=1;j<8;j++)
+       {
+           if(arr[j]==arr[i])
+              {
+                   count[arr[i]]++;
+                   break;
+              }
 
-    /* Input elements in second matrix from user */
-    printf("\nEnter elements in matrix B of size %dx%d: \n", SIZE, SIZE);
-    for(row=0; row<SIZE; row++)
-    {
-        for(col=0; col<SIZE; col++)
-        {
-            scanf("%d", &B[row][col]);
-        }
-    }
-  for(row=0; row<SIZE; row++)   //Multiply both matrices A*B
-    {
-        for(col=0; col<SIZE; col++)
-        {
-            sum = 0;
-            /*
-             * Multiply row of first matrix to column of second matrix
-             * and store sum of product of elements in sum.
-             */
-            for(k=0; k<SIZE; k++)
-            {
-                sum =sum+ A[row][k] * B[k][col];
-            }
+       }
+   }
+   printf("\n\n First NonRepetitive element=");
+     for(i=0;i<8;i++)
+     {
+         if(count[arr[i]]==1)
+         {
+             printf("%d \n",arr[i]);
+             break;
+         }
 
-            C[row][col] = sum;
-        }
-    }
-
-    /* Print product of the matrices */
-    printf("\nProduct of matrix A * B = \n");
-    for(row=0; row<SIZE; row++)
-    {
-        for(col=0; col<SIZE; col++)
-        {
-            printf("%d ", C[row][col]);
-        }
-        printf("\n");
-    }
-return 0;
+     }
+    return 0;
 }
